984627cab79e6f76262dd2baf631860bff1d188f
canbula <bora.canbula@cbu.edu.tr>
2021-12-17 00:37:59 +0300

    Animated plot added

--- NewtonRaphson.py
+++ NewtonRaphson.py
@@ -5,27 +5,74 @@ from matplotlib.animation import FuncAnimation
 
 
 class NewtonRaphson(FuncAnimation):
-    def __init__(self, f, x, h, iteration_limit):
-        self.f = f
+    def __init__(self, func, x, h, tolerance, iteration_limit, x_limits, verbose=False):
+        self.f = func
         self.x = x
         self.h = h
+        self.tolerance = tolerance
+        self.satisfied = False
         self.iteration_limit = iteration_limit
+        self.x_limits = x_limits
+        self.verbose = verbose
         matplotlib.use("TkAgg")
         plt.rcParams["figure.figsize"] = [24, 18]
         self.fig, self.ax = plt.subplots()
-        super(NewtonRaphson, self).__init__(self.fig, self.update_animation, interval=500)
+        super(NewtonRaphson, self).__init__(self.fig, self.update_animation,
+                                            init_func=self.init_animation, interval=500)
         plt.show()
 
+    def init_animation(self):
+        pass
+
     def update_animation(self, frame):
-        if frame > self.iteration_limit:
-            quit()
-        self.x = self.next_root()
+        if self.satisfied or frame >= self.iteration_limit:
+            plt.close()
         self.ax.clear()
         self.ax.set_title(f"Newton Raphson Method: Iteration={frame} @ x={self.x:.6f}")
+        x_points = np.linspace(self.x_limits[0], self.x_limits[1], self.x_limits[2])
+        self.ax.set_xlim(self.x_limits[0], self.x_limits[1])
+        self.ax.set_ylim(self.f(self.x_limits[0]), self.f(self.x_limits[1]))
+        self.ax.plot(x_points, self.f(x_points), linewidth=2, color="red")
+        self.ax.plot(x_points, [0 for _ in x_points], linewidth=1, color="black")
+        self.ax.plot([self.x, self.x], [0, self.f(self.x)], linewidth=1, color="blue")
+        self.ax.plot(self.x, self.f(self.x), marker="o", markersize=10)
+        self.ax.plot([self.x, self.next_root()], [self.f(self.x), 0], linewidth=1, color="green")
+        self.x = self.next_root()
+        if self.verbose:
+            print(f"Iteration={frame} @ x={self.x:.6f}")
 
     def diff(self):
         return (self.f(self.x+self.h) - self.f(self.x))/self.h
 
     def next_root(self):
-        return self.x - self.f(self.x)/self.diff()
+        term = self.f(self.x)/self.diff()
+        if term < self.tolerance:
+            self.satisfied = True
+        return self.x - term
 
+
+def f(x):
+    return x**2 - 4*x - 7
+
+
+def g(x):
+    return 27*x**3 - 3*x + 1
+
+
+def main():
+    x = 9
+    h = 1e-6
+    tolerance = 1e-3
+    iteration_limit = 10
+    x_limits = [2, 8, 100]
+    NewtonRaphson(f, x, h, tolerance, iteration_limit, x_limits, True)
+    x = 5
+    h = 1e-6
+    tolerance = 1e-6
+    iteration_limit = 30
+    x_limits = [-1, 1, 100]
+    NewtonRaphson(g, x, h, tolerance, iteration_limit, x_limits, True)
+
+
+if __name__ == "__main__":
+    main()
